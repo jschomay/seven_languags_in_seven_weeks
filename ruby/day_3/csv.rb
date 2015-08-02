29902a4a10313cac56d1a5152482cb3fa89ddb13
Jeff Schomay <jschomay@gmail.com>
2015-08-02 17:35:53 -0600

    Add ruby csv example from book

---
+++ ruby/day_3/csv.rb
@@ -0,0 +1,38 @@
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+    def initialize
+      read
+    end
+  end
+end
+
+class OzCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+oz = OzCsv.new
+puts oz.headers.inspect
+puts oz.csv_contents.inspect
