c2ca84e3b7c7c436d5c96ab397f665b9f9cab789
Jeff Schomay <jschomay@gmail.com>
2015-08-08 21:20:53 -0600

    better csv module with missing method

--- ruby/day_3/csv.rb
+++ ruby/day_3/csv.rb
@@ -3,6 +3,23 @@ module ActsAsCsv
     base.extend ClassMethods
   end
 
+  class CsvObject
+    def initialize(headers, contents)
+      @headers = headers
+      @contents = contents
+    end
+
+    # def one
+    #   @contents.map { |row| row[0] }
+    # end
+
+    def method_missing(name, *args, &block)
+      # todo - defined missing method to pull the right index from each contents
+      index =  @headers.index(name.to_s)
+      @contents.map { |row| row[index] }
+    end
+  end
+
   module ClassMethods
     def acts_as_csv
       include InstanceMethods
@@ -13,12 +30,16 @@ module ActsAsCsv
     def read
       @csv_contents = []
       filename = self.class.to_s.downcase + '.txt'
-      file = File.new(filename)
-      @headers = file.gets.chomp.split(', ')
+      @file = File.new(filename)
+      @headers = @file.gets.chomp.split(', ')
 
-      file.each do |row|
+      @file.each do |row|
         @csv_contents << row.chomp.split(', ')
       end
+    end
+
+    def each
+      yield CsvObject.new @headers, @csv_contents
     end
 
     attr_accessor :headers, :csv_contents
@@ -36,3 +57,5 @@ end
 oz = OzCsv.new
 puts oz.headers.inspect
 puts oz.csv_contents.inspect
+puts "Just the first ones now:"
+oz.each { |row| puts row.one }
